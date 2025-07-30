00c5475dc74a7c04f67330fd6774c1f44fd65301
Darsh Parikh <parikhdarsh121@gmail.com>
2025-07-30 21:13:06 +0530

    Fixed the size changing of the rubik's cube

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -1,33 +1,49 @@
 import type React from 'react';
-import { useContext } from 'react';
+import { useEffect, useMemo } from 'react';
 import * as THREE from 'three';
-import AppContext from '../context/AppContext';
 import { getFaceColor } from '../utils/getFaceColor';
+import { useAppContext } from '../hooks/useAppContext';
 
 interface CubeProps {
     position: [number, number, number];
 }
 
 const Cube: React.FC<CubeProps> = ({ position }) => {
-    const context = useContext(AppContext);
-
-    if (!context) return null;
-
-    const { size, cube } = context;
+    const { size, cube } = useAppContext();
     const [x, y, z] = position;
 
-    const faceColors = getFaceColor(x, y, z, size, cube);
+    const materials = useMemo(() => {
+        const faceColors = getFaceColor(x, y, z, size, cube);
+        return faceColors.map(color => new THREE.MeshBasicMaterial({ color }));
+    }, [x, y, z, size]);
 
-    const materials = faceColors.map((color) => (
-        new THREE.MeshBasicMaterial({ color })
-    ));
+    // Update material colors when cube state changes (but not size, as that recreates materials)
+    useEffect(() => {
+        const faceColors = getFaceColor(x, y, z, size, cube);
+        
+        faceColors.forEach((color, index) => {
+            if (materials[index]) {
+                materials[index].color.copy(color);
+                materials[index].needsUpdate = true;
+            }
+        });
+    }, [cube, materials]);
 
-    const geometry = new THREE.BoxGeometry(0.9, 0.9, 0.9);
-    const edges = new THREE.EdgesGeometry(geometry);
+    const geometry = useMemo(() => new THREE.BoxGeometry(0.9, 0.9, 0.9), []);
+    const edges = useMemo(() => new THREE.EdgesGeometry(geometry), [geometry]);
+
+    useEffect(() => {
+        return () => {
+            materials.forEach(material => material.dispose());
+        };
+    }, [materials]);
 
     return (
         <group position={position}>
-            <mesh geometry={geometry} material={materials} />
+            <mesh 
+                geometry={geometry} 
+                material={materials}
+            />
             <lineSegments geometry={edges}>
                 <lineBasicMaterial color={0x000000} />
             </lineSegments>
